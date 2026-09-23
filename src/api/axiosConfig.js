ba54cec027962ea78b7077c306e4b170223c9c27
Ruth <ruthchukwu766@gmail.com>
2026-09-23 12:10:02 +0100

    Update axios base URL to new server address

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL:'http://3.137.178.136:8080',
     headers: {
         'Content-Type': 'application/json',
     },
